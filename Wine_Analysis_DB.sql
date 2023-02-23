3ad38852b59d19aa424de729276b74ea1899b485
natasharrosario <natasharoquerosario@gmail.com>
2023-02-23 17:42:55 -0500

    Uploading database and queries

--- Wine_Analysis_DB.sql
+++ Wine_Analysis_DB.sql
@@ -15,3 +15,47 @@ create table wine_DB (
 	variety varchar,
 	winery varchar
 );
+
+-- Sorting data into different tables grouped by relevant information.
+
+-- Wine grouped by variety
+select title, variety
+into wine_by_variety
+from wine_db;
+
+select * from wine_by_variety;
+
+-- Wine grouped by description
+select title, description
+into wine_by_description
+from wine_db;
+
+select * from wine_by_description;
+
+-- Wine grouped by score
+select title, points
+into wine_by_score
+from wine_db;
+
+select * from wine_by_score;
+
+-- Wine grouped by price
+select title, price
+into wine_by_price
+from wine_db;
+
+select * from wine_by_price;
+
+-- Wine grouped by location
+select title, country, province, region_1, region_2, winery, designation
+into wine_by_location
+from wine_db;
+
+select * from wine_by_location;
+
+-- Wine grouped by taster info
+select title, taster_name, taster_twitter, points
+into wine_by_taster_info
+from wine_db;
+
+select * from wine_by_taster_info;
