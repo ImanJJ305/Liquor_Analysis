d5c9cc8f4929ee67214bde9401030c8350f9f999
natasharrosario <natasharoquerosario@gmail.com>
2023-02-23 17:07:28 -0500

    Importing data into table and cleaning CSV file

--- Wine_Analysis_DB.sql
+++ Wine_Analysis_DB.sql
@@ -1,18 +1,17 @@
 -- Creating tables for Winery-Analysis Database
 
 create table wine_DB (
-	country varchar not null,
-	description varchar not null,
-	designation varchar not null,
-	points int not null,
-	price int not null,
-	province varchar not null,
-	region_1 varchar not null,
-	region_2 varchar not null,
-	taster_name varchar not null,
-	taster_twitter varchar not null,
+	country varchar,
+	description varchar,
+	designation varchar,
+	points int,
+	price int,
+	province varchar,
+	region_1 varchar,
+	region_2 varchar,
+	taster_name varchar,
+	taster_twitter varchar,
 	title varchar not null,
-	variety varchar not null,
-	winery varchar not null,
-	primary key (title)
+	variety varchar,
+	winery varchar
 );
